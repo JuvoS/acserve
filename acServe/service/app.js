bcf871fb8c50770fc51ad6859ccbc3140476063f
JuvoS <www.704921698@qq.com>
2019-11-12 00:34:01 +0800

    add db

--- acServe/service/app.js
+++ acServe/service/app.js
@@ -1,49 +1,48 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var ejs = require('ejs') ;
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var ejs = require("ejs");
 
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var entryRouter = require("./routes/entry");
+var uploadRouter = require("./routes/upload");
+var addressRouter = require("./routes/address");
+var billRouter = require("./routes/bill");
+var billServeRouter = require("./routes/billserve");
+var deviceRouter = require("./routes/device");
+var suggestRouter = require("./routes/suggest");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var entryRouter = require('./routes/entry');
-var uploadRouter = require('./routes/upload');
-var addressRouter = require('./routes/address');
-var billRouter = require('./routes/bill');
-var billServeRouter = require('./routes/billserve');
-var deviceRouter = require('./routes/device');
-var suggestRouter = require('./routes/suggest');
-
-var manageRouter = require('./routes/manage');
-var smsRouter = require('./routes/sms');
+var manageRouter = require("./routes/manage");
+var smsRouter = require("./routes/sms");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 // app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/entry', entryRouter);
-app.use('/upload', uploadRouter);
-app.use('/address', addressRouter);
-app.use('/bill', billRouter);
-app.use('/billServe', billServeRouter);
-app.use('/device', deviceRouter);
-app.use('/suggest', suggestRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/entry", entryRouter);
+app.use("/upload", uploadRouter);
+app.use("/address", addressRouter);
+app.use("/bill", billRouter);
+app.use("/billServe", billServeRouter);
+app.use("/device", deviceRouter);
+app.use("/suggest", suggestRouter);
 
-app.use('/manage', manageRouter);
-app.use('/sms', smsRouter);
+app.use("/manage", manageRouter);
+app.use("/sms", smsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -54,11 +53,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
